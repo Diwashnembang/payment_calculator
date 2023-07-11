30ab0df9d244144465c3ac9db51fc32791880c35
diwash nembang <diwashnembang0@gmail.com>
2023-07-11 20:40:45 +1000

    added time

--- src/components/Status.js
+++ src/components/Status.js
@@ -1,10 +1,25 @@
 import { Box, HStack } from "@chakra-ui/react";
+import React, { useEffect, useState } from "react";
 
 const Status = ({ pay, totalPay, setTotalPay }) => {
+  const [time, setTime] = useState("0:0");
+  const date = new Date();
+
+  useEffect(() => {
+    setTimeout(() => {
+      let hrs = date.getHours();
+      let min = date.getMinutes();
+      let sec = date.getSeconds();
+      console.log("running in status");
+      setTime(`time : ${hrs} : ${min} : ${sec}`);
+    }, 1000);
+  }, [time]);
+
   return (
     <HStack>
       <Box id="total-earned">Total pay 0</Box>
       <Box id="hours-worked">Hours worked0</Box>
+      <Box>time {time} </Box>
     </HStack>
   );
 };
